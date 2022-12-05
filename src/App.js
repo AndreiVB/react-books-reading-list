68a50bcd60a059b2a4ba566640743f69cb9019b3
AndreiWWW <andreiv.banut@gmail.com>
2022-12-05 18:24:32 +0200

    created state & props system for all components
    combined thow handlers into one in BookShow -> handleSubmit event handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App () {
     const [books, setBooks] = useState([]);
 
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if(book.id === id) {
+                return { ...book, title: newTitle }
+            }
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const deleteBookbyId = (id) => {
+        //filter doesn't modifie the array but gives a new one
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        })
+
+        setBooks(updatedBooks);
+    }
+
 	const createBook = (title) => {
-			console.log('add book with title:', title);
+			const updatedBooks = [
+                ...books,
+                { id: Math.round(Math.random() * 9999), title}
+            ];
+            setBooks(updatedBooks);
 	};
 
-    return <div>
-        <BookCreate onCreate={createBook}/>
-    </div>
+    return (
+			<div className="app">
+                <h1>Reading list</h1>
+				<BookList
+					books={books}
+					onEdit={editBookById}
+					onDelete={deleteBookbyId}
+				/>
+				<BookCreate onCreate={createBook} />
+			</div>
+		);
 };
 
 export default App;
