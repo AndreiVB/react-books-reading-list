464093f0ca20c1c2133065d9767f9f45ed806708
AndreiWWW <andreiv.banut@gmail.com>
2022-12-06 13:02:02 +0200

    created fetchBooks() & use of useEffect(), updated editBookById() & deleteBookById() to async functions
    added some comments to useEffect() use casses

--- src/App.js
+++ src/App.js
@@ -1,52 +1,68 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App () {
-    const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
-        const updatedBooks = books.map((book) => {
-            if(book.id === id) {
-                return { ...book, title: newTitle }
-            }
+	const fetchBooks = async () => {
+		const response = await axios.get("http://localhost:3001/books");
 
-            return book;
-        });
+		setBooks(response.data);
+	};
+	//useEffect() calls posibilities; second parameter ->
+	//1) empty array -> getting called at first render & never gets called again
+	//2) no second argument -> gets called at every rerender
+	//3) second argument is a variable -> gets called at every change of the given variable
+	//4) second argument array with multiple variables -> gets called at every change of a variable from the array
+	useEffect(() => {
+		fetchBooks();
+	}, []);
 
-        setBooks(updatedBooks);
-    };
+	const editBookById = async (id, newTitle) => {
+		const response = await axios.put(`http://localhost:3001/books/${id}`, {
+			title: newTitle
+		});
 
-    const deleteBookbyId = (id) => {
-        //filter doesn't modifie the array but gives a new one
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        })
+		const updatedBooks = books.map((book) => {
+			if (book.id === id) {
+				//updates all data that have been changed
+				return { ...book, ...response.data };
+			}
 
-        setBooks(updatedBooks);
-    }
+			return book;
+		});
 
-	const createBook = async (title) => {
-        const response = await axios.post('http://localhost:3001/books', {
-            title
-        });
-        
-		const updatedBooks = [...books, response.data];
-        setBooks(updatedBooks);
+		setBooks(updatedBooks);
 	};
 
-    return (
-			<div className="app">
-                <h1>Reading list</h1>
-				<BookList
-					books={books}
-					onEdit={editBookById}
-					onDelete={deleteBookbyId}
-				/>
-				<BookCreate onCreate={createBook} />
-			</div>
-		);
+	const deleteBookbyId = async (id) => {
+		await axios.delete(`http://localhost:3001/books/${id}`);
+		//filter doesn't modify the array but gives a new one
+		const updatedBooks = books.filter((book) => {
+			return book.id !== id;
+		});
+
+		setBooks(updatedBooks);
+	};
+
+	const createBook = async (title) => {
+		const response = await axios.post("http://localhost:3001/books", {
+			title,
+		});
+
+		const updatedBooks = [...books, response.data];
+		setBooks(updatedBooks);
+	};
+
+	return (
+		<div className="app">
+			<h1>Reading list</h1>
+			<BookList books={books} onEdit={editBookById} onDelete={deleteBookbyId} />
+			<BookCreate onCreate={createBook} />
+		</div>
+	);
 };
 
 export default App;
